00413359c9c8d83cb022e771bb4598d166bff154
Gaurav Saini <me@gauravsaini.com>
2021-07-02 16:38:19 +1000

    fix : remove analytics

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,5 +1,5 @@
 import TagManager from './lib/tagmanager';
-import Analytics from './lib/analytics';
+// import Analytics from './lib/analytics';
 import Optimize from './lib/optimize';
 
 function plugin({ setHeadComponents, setPreBodyComponents, setPostBodyComponents }, pluginOptions = {}) {
@@ -7,14 +7,14 @@ function plugin({ setHeadComponents, setPreBodyComponents, setPostBodyComponents
   if ( process.env.NODE_ENV !== 'production' && !pluginOptions.includeInDevelopment ) return false;
 
   const tagmanager = new TagManager(pluginOptions.tagmanager, pluginOptions.dataLayer);
-  const analytics = new Analytics(pluginOptions.analytics, pluginOptions.optimize && pluginOptions.optimize.id);
+  // const analytics = new Analytics(pluginOptions.analytics, pluginOptions.optimize && pluginOptions.optimize.id);
   const optimize = new Optimize(pluginOptions.optimize, pluginOptions.tagmanager && pluginOptions.tagmanager.id);
 
   const newHeadElements = [
     tagmanager.dataLayer(),
-    analytics.setup(),
+    // analytics.setup(),
     optimize.asyncHide(),
-    analytics.script(),
+   // analytics.script(),
     tagmanager.script(),
   ].filter(el => el !== null);
   setHeadComponents(newHeadElements);
